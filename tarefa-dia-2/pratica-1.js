2f35ea66478cca0ed064f7c12c7895f3a1d0b836
Gilmar <gilmarschmitz89@gmail.com>
2022-08-30 20:37:34 -0300

    pratica 1 alteracao

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,9 @@
-let nome = "Gilmar"
-let email = "gilmarschmitz89@gmail.com"
-let profissao = "programador de supervisorio"
-let idade = "33"
-let local = "Paris"
+let nome =  "Gilmar"
+let email =  "gilmarschmitz89@gmail.com"
+let profissao =  "programador de supervisorio"
+let idade =  33
+let local =  "Paris"
+
+let mensagem = " meu nome " + nome + " meu email " + email + " minha profissão " + profissao + " minha idade " + idade + " para onde quero viajar " + local + " fim. " 
+
+console.log(mensagem)
