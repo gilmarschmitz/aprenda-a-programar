f9821a4e4f71b3138739bb6bb029e9c2d3f46c6b
Gilmar <gilmarschmitz89@gmail.com>
2022-08-30 21:05:51 -0300

    pratica 1 alterando e resolvendo texto

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -4,6 +4,6 @@ let profissao =  "programador de supervisorio"
 let idade =  33
 let local =  "Paris"
 
-let mensagem = " meu nome " + nome + " meu email " + email + " minha profissão " + profissao + " minha idade " + idade + " para onde quero viajar " + local + " fim. " 
+let mensagem = " Olá, " + nome + " !  Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + local + ", Confirma pra gente alguns dados? A sua idade é " + idade + " anos, você é " + profissao + " e o seu email é " + email + " ?  Ficamos no aguardo. Muito obrigado e boa viagem! :D "
 
 console.log(mensagem)
